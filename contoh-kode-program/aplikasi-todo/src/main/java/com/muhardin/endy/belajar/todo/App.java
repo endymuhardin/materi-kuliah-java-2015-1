c402353114be9ed74eac40dbc04049d6f2eab4ec
Endy Muhardin <endy.muhardin@gmail.com>
2015-03-26 20:38:11 +0700

    menggunakan library joda time

--- contoh-kode-program/aplikasi-todo/src/main/java/com/muhardin/endy/belajar/todo/App.java
+++ contoh-kode-program/aplikasi-todo/src/main/java/com/muhardin/endy/belajar/todo/App.java
@@ -1,11 +1,13 @@
 package com.muhardin.endy.belajar.todo;
 
-/**
- * Hello world!
- *
- */
+import org.joda.time.DateTime;
+
 public class App {
     public static void main( String[] xyz ) {
         System.out.println( "Hello World!" );
+        DateTime sekarang = new DateTime();
+        System.out.println("Tanggal hari ini : "+sekarang);
+
+        System.out.println("10 hari kemudian : "+sekarang.plusDays(10));
     }
 }
