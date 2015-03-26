3f1e8c828f15ae6b7d7f3797a64bcf068ac74c3a
Endy Muhardin <endy.muhardin@gmail.com>
2015-03-26 20:13:03 +0700

    contoh project Maven

---
+++ contoh-kode-program/aplikasi-todo/src/main/java/com/muhardin/endy/belajar/todo/App.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.todo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] xyz )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
