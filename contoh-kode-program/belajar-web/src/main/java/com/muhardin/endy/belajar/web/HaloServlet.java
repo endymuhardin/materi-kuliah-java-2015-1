f1d70da374b36a8a4078da5d0c7e887420dac74a
Endy Muhardin <endy.muhardin@gmail.com>
2015-05-07 20:46:28 +0700

    contoh servlet

---
+++ contoh-kode-program/belajar-web/src/main/java/com/muhardin/endy/belajar/web/HaloServlet.java
@@ -0,0 +1,31 @@
+package com.muhardin.endy.belajar.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/halo.php")
+public class HaloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+                                       throws ServletException, IOException {
+        PrintWriter output = resp.getWriter();
+        
+        resp.setContentType("text/html");
+        
+        String nama = req.getParameter("nama");
+        
+        output.println("<html>");
+        output.println("<head>");
+        output.println("<title>HaloServlet</title>");
+        output.println("</head>");
+        output.println("<body>");
+        output.println("<h1>Halo "+nama+"</h1>");
+        output.println("</body>");
+        output.println("</html>");
+        output.flush();
+    }
+}
